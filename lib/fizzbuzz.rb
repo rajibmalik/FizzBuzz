36a4a3759635aa0797f4a369e171ef9bde637239
rajibmalik <rajibmalik@hotmail.co.uk>
2022-09-08 14:07:47 +0000

    Added requirement to return number if not fizz buzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -3,7 +3,7 @@ def fizzbuzz(number)
       "fizz"
     elsif number % 5 == 0 
       "buzz"
-    else 
+    else
       number 
     end 
 end 
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,9 +1,5 @@
 require 'fizzbuzz'
 describe 'fizzbuzz' do
-  it 'returns "fizzbuzz" when given a number divisible by 3 and 5' do
-    expect(fizzbuzz(15)).to eq 'fizzbuzz'
-  end
-
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
@@ -11,4 +7,8 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
+  
+  it 'returns the number when given a number which is not divisble by 3 or 5' do 
+    expect(fizzbuzz(2)).to eq(2) 
+  end 
 end
