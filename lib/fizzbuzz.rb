f5cdac958f40f1c7514aa00c83b60b28347511b8
Ollie Beck <ollieb94@hotmail.co.uk>
2022-09-08 15:55:05 +0100

    Fizzbuzz test working with additional examples

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,26 @@
+=begin 
 def fizzbuzz(number)
-    if number % 3 == 0 
-      "fizz"
-    elsif number % 5 == 0 
-      "buzz"
-    else
-      number 
-    end 
+  if number % 3 == 0 && number % 5 == 0
+    "fizzbuzz"
+  elsif number % 3 == 0 
+    "fizz"
+  elsif number % 5 == 0 
+    "buzz"
+  else
+    number 
+  end 
 end 
+=end
+
+=begin
+(not working)
+def fizzbuzz(number)
+  number % 3 == 0 ? "fizz" : number
+  number % 5 == 0 ? "buzz" : number
+  number % 3 == 0 && number % 5 == 0 ? "fizzbuzz" : number
+end
+=end
+
+def fizzbuzz(number)
+  (number % 3 == 0) ? ((number % 5 == 0) ? "fizzbuzz" : "fizz") : ((number % 5 == 0) ? "buzz" : number)
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,9 @@
 require 'fizzbuzz'
 describe 'fizzbuzz' do
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
